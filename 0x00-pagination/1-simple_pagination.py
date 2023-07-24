fc51fa2bd8dd87ce1ee0ee9ba969c4db2cb8ffad
leulkashay <leulkashay2012@gmail.com>
2023-07-24 07:21:12 +0000

    new

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -3,14 +3,14 @@
 Pagination
 """
 import csv
+import math
 from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int]:
     """
-    a function named index_range that
-    takes two integer argument page
-    and page_sizr and return atuple
+    return start index and end index
+    corresponding to the range of indexes
     """
     start_index = (page - 1) * page_size
     end_index = page * page_size
@@ -18,10 +18,9 @@ def index_range(page: int, page_size: int) -> Tuple[int]:
     return (start_index, end_index)
 
 
-
 class Server:
     """
-    Server class to paginate adataset popular by baby names.
+    Server class to paginate a database of popular baby names.
     """
     file_data = "Popular_Baby_Names.csv"
 
@@ -40,9 +39,9 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page: int=1, page_size: int=10) -> List[List]:
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
-        paginate the dataset
+        Paginate the dataset
         """
         assert type(page) == int and page > 0
         assert type(page_size) == int and page_size > 0
