a9238ca3b825540ecc1800c5179191c21f31c648
leulkashay <leulkashay2012@gmail.com>
2023-07-26 19:26:44 +0000

    new task

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env pytgon3
+"""
+LRU caching
+"""
+from base_caching import BaseCaching
+
+
+class LRUCaache(BaseCaching):
+    """
+    Inherits from basecaching
+    """
+    def __init___(self):
+        """
+        initilaze LRU caching
+        """
+        super().__init__()
+        self.queue = []
+
+    def put(self, key,  item):
+        """
+        Assiging key and items
+        """
+        if len(self.cache_data) == self.MAX_ITEMS and key not in self.queue:
+            discard = self.queue.pop()
+            del self.cache_data[discard]
+            print("DiSCARD: {}".format(discard))
+
+        if key and item:
+            if key in self.cache_data:
+                self.queue.remove(key)
+            self.queue.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Fetch data with key
+        """
+        if not key or key not in self.cache_data:
+            return None
+        # Remove from any position in the queue and add to the back
+        self.queue.remove(key)
+        self.queue.append(key)
+
+        return self.cache_data[key]
