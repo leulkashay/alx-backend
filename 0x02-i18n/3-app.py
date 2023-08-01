3861e4aaa862e6633d82c39ef6ba3f3cab70d00b
leulkashay <leulkashay2012@gmail.com>
2023-08-01 10:37:36 +0000

    task do

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+a simple flask app
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    class used to configure languages
+    """
+    LANGUAGES = ['en', 'fr']
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route("/")
+def hello_world():
+    """
+    parametersize
+    """
+    title = _("home_title")
+    header = _("home_header")
+    return render_template("3-index.html", title=title, header=header)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    get locale
+    """
+    locale = request.accept_languages.best_match(app.config['LANGUAGES'])
+    return locale
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
